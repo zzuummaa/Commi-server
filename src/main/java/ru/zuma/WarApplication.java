dc19f61d5a87d3dc582cf6136bc3ea4bcc26cc31
tohaheleneto <anton_mamont@bk.ru>
2018-07-26 16:29:01 +0300

    warApplication updated

--- src/main/java/ru/zuma/WarApplication.java
+++ src/main/java/ru/zuma/WarApplication.java
@@ -3,7 +3,7 @@ package ru.zuma;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class WarApplication extends SpringBootServletInitializer {
